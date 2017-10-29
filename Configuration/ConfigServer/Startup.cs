4a19f6e8777d21274c6cb323330006a9c61a87ce
Erik Xu <81636759@qq.com>
2017-10-29 23:57:54 +0800

    Init.

---
+++ Configuration/ConfigServer/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Dotnet.Microservice;
+using Dotnet.Microservice.Netcore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Extensions.Configuration;
+
+namespace ConfigServer
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+                .AddEnvironmentVariables()
+                .AddConfigServer(env);
+
+            Configuration = builder.Build();
+
+            AppConfig.AddConfigurationObject(Configuration, "all");
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseMvc();
+            app.UseEnvironmentEndpoint();
+        }
+    }
+}
---
+++ Configuration/Volume/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Dotnet.Microservice;
+using Dotnet.Microservice.Netcore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Volume
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+                .AddEnvironmentVariables()
+                .AddJsonFile("/configs/volume.json", true, true);
+
+            Configuration = builder.Build();
+
+            AppConfig.AddConfigurationObject(Configuration, "all");
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseMvc();
+            app.UseEnvironmentEndpoint();
+        }
+    }
+}
